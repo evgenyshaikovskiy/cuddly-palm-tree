23dfbb9ddbab9e88e79d714328d933c3b7a12138
evgenyshaikovskiy <evgenyshaikovskiy@gmail.com>
2022-02-26 16:41:44 -0800

    feat: implement fuel tank

--- src/models/fuel_tank.py
+++ src/models/fuel_tank.py
@@ -3,7 +3,12 @@ from abstractions.fuel_tank import AbstractFuelTank
 
 class FuelTank(AbstractFuelTank):
 
-    def __init__(self, fillLevel=20, tankSize=60):
+    def __init__(self, fillLevel=20, tankSize=60, onReserveBorder=5):
+        # declare private variables with float types
+        self.__fillLevel: float
+        self.__tankSize: float
+        self.__onReserveBorder: float
+
         if fillLevel < 0:
             self.__fillLevel = 0
 
@@ -12,3 +17,28 @@ class FuelTank(AbstractFuelTank):
 
         self.__fillLevel = fillLevel
         self.__tankSize = tankSize
+
+    @property
+    def FillLevel(self):
+        return self.__fillLevel
+
+    @property
+    def IsOnReserve(self):
+        return self.__fillLevel < self.__onReserveBorder
+
+    @property
+    def IsFull(self):
+        return self.__fillLevel == self.__tankSize
+
+    def Consume(self, liters: float):
+        self.__fillLevel -= liters
+        self.__fillLevel = round(self.__fillLevel, 10)
+
+        if self.__fillLevel < 0:
+            self.__fillLevel = 0
+
+    def Refuel(self, liters: float):
+        self.__fillLevel += liters
+
+        if self.__fillLevel > self.__tankSize:
+            self.__fillLevel = self.__tankSize
