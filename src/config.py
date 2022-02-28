c2440dd62f71d80c1cf9ff04742007fc105388e4
evgenyshaikovskiy <evgenyshaikovskiy@gmail.com>
2022-02-27 22:52:16 -0800

    add config file

---
+++ src/config.py
@@ -0,0 +1,32 @@
+class config():
+    @property
+    def DefaultFillLevel():
+        return 20
+
+    @property
+    def DefaultTankSize():
+        return 60
+
+    @property
+    def DefaultOnReserveBorder():
+        return 5
+
+    @property
+    def DefaultMaxAccelerationRatio():
+        return 20
+
+    @property
+    def DefaultMinAccelerationRatio():
+        return 5
+
+    @property
+    def DefaultMaxSpeed():
+        return 250
+
+    @property
+    def DefaultBrakingSpeed():
+        return 10
+    
+    @property
+    def AccelerationRatio():
+        return 10
