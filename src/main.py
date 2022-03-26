fd900988ecb722cb4f663f078e7716d5351b15c6
Yauheni Shaikouski <evgenyshaikovskiy@gmail.com>
2022-03-26 11:25:15 +0300

    replace main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,55 @@
+import asyncio
 from models.car import Car
 
-car: Car = Car()
 
-car.EngineStart()
-car.GetInformationOnCar()
-car.Refuel(10)
-car.Accelerate(10)
-car.GetInformationOnCar()
+async def main():
+    # create default car modeling.
+    car: Car = Car()
+
+    while (True):
+        print('''Choose action:
+              1 - Start Engine.
+              2 - Stop Engine.
+              3 - Run Idle.
+              4 - Free Wheel.
+              5 - Brake by certain speed.
+              6 - Accelerate by certain speed.
+              7 - Refuel Car.
+              8 - Get information about car condition.
+              To cancel programm press Space.
+              ''')
+        action = int(input())
+
+        if action == 1:
+            car.EngineStart()
+            await asyncio.sleep(2)
+        elif action == 2:
+            car.EngineStop()
+            await asyncio.sleep(2)
+        elif action == 3:
+            car.RunningIdle()
+            await asyncio.sleep(3)
+        elif action == 4:
+            car.FreeWheel()
+            await asyncio.sleep(3)
+            print('Input count of km/h to brake by')
+        elif action == 5:
+            speed = int(input())
+            car.BrakeBy(speed)
+            await asyncio.sleep(3)
+        elif action == 6:
+            print('Input count of km/h to accelerate by')
+            speed = int(input())
+            car.Accelerate(speed)
+            await asyncio.sleep(3)
+        elif action == 7:
+            print('Input amount of liters to refuel')
+            liters = float(input())
+            car.Refuel(liters)
+            await asyncio.sleep(3)
+        elif action == 8:
+            car.GetInformationOnCar()
+            await asyncio.sleep(3)
+
+
+asyncio.run(main())
