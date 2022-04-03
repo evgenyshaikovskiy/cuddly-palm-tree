10cb5b9f50dc12ff3a86ece348824486baa553c0
Yauheni Shaikouski <evgenyshaikovskiy@gmail.com>
2022-04-04 01:15:24 +0300

    update config

--- src/utility/config.py
+++ src/utility/config.py
@@ -1,90 +1,12 @@
+import json
+import string
+
+
+with open('src/utility/config.json', 'r') as config_file:
+    data = json.load(config_file)
 
 
 class config():
     @staticmethod
-    def default_fill_level():
-        return 20
-
-    @staticmethod
-    def default_tank_size():
-        return 60
-
-    @staticmethod
-    def min_tank_size():
-        return 20
-
-    @staticmethod
-    def default_on_reserve_border():
-        return 5
-
-    @staticmethod
-    def default_max_acceleraton_ratio():
-        return 20
-
-    @staticmethod
-    def default_min_max_acceleration_ratio():
-        return 1
-
-    @staticmethod
-    def default_min_acceleration_ratio():
-        return 5
-
-    @staticmethod
-    def default_max_speed():
-        return 250
-
-    @staticmethod
-    def default_min_speed():
-        return 10
-
-    @staticmethod
-    def max_braking_speed():
-        return 30
-
-    @staticmethod
-    def min_braking_speed():
-        return 5
-
-    @staticmethod
-    def default_braking_speed():
-        return 10
-
-    @staticmethod
-    def default_acceleration_ratio():
-        return 10
-
-    @staticmethod
-    def default_car_coefficient():
-        return 1
-
-    @staticmethod
-    def default_acceleration_coefficient():
-        return 1.3
-
-    @staticmethod
-    def default_braking_coefficient():
-        return 0.1
-
-    @staticmethod
-    def default_running_idle_consumption_rate():
-        return 0.001
-
-    @staticmethod
-    def default_running_quater_consumption_rate():
-        return 0.006
-
-    @staticmethod
-    def default_running_half_consumption_rate():
-        return 0.012
-
-    @staticmethod
-    def default_running_upper_half_consumption_rate():
-        return 0.024
-
-    @staticmethod
-    def default_running_before_max_consumption_rate():
-        return 0.048
-
-    @staticmethod
-    def default_running_max_speed_consumption_rate():
-        return 0.096
+    def get(key: string):
+        return data[key]
